dd02729c08f2cc8053fe907ac2c6e36284045192
Jesse Zonneveld <jesse@pilotfish.ca>
2022-11-24 14:50:37 +0000

    Add eslint, ts, and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 9,
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: ['**/*.js'],
+};
